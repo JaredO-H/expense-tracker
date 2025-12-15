b1f7daff8da10dc3315448a119f6085f2d57578a
Jared <Jared@gjspace.co.uk>
2025-12-15 17:59:38 +0000

    the fab overlay on the ExpensesScreen.tsx is now transparent

--- ExpenseTracker/src/screens/expenses/ExpensesScreen.tsx
+++ ExpenseTracker/src/screens/expenses/ExpensesScreen.tsx
@@ -317,7 +317,7 @@ const styles = StyleSheet.create({
   },
   fabOverlay: {
     ...StyleSheet.absoluteFillObject,
-    backgroundColor: colors.blackOverlay50,
+    backgroundColor: 'transparent',
     zIndex: 998,
   },
   fabMenuContainer: {
